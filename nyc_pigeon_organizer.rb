6e3cc92238d2174ecd32efff3127198cc8b66172
Catrina Friday <cat.friday@gmail.com>
2020-05-13 13:44:33 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_2 = {}
+
+  data.each do |key, value|
+    value.each  do |attribute, names|
+      names.each  do |name|
+        
+        if !pigeon_2[name]
+          pigeon_2[name] = {}
+        end
+
+        if !pigeon_2[name][key]
+          pigeon_2[name][key] = []
+        end
+
+        pigeon_2[name][key] << attribute.to_s   
+        
+      end
+      
+    end
+  end
+  pigeon_2
 end
